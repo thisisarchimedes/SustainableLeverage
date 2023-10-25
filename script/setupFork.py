45e94657c38e59c33d6008d1e73c31e73a108872
aliberkhsl <aliberkhasil@gmail.com>
2023-10-26 01:42:55 +0300

    hardcode node address

--- script/setupFork.py
+++ script/setupFork.py
@@ -4,9 +4,9 @@ import sys
 
 
 def set_wbtc_balance_of_vault(storageSlot, wbtc_address, amount):
-    aws_fork_url = os.environ["AWS_FORK_URL"]
+    aws_fork_url = "http://ec2-54-211-119-50.compute-1.amazonaws.com:8545"
     requests.post(
-        aws_fork_url,
+        url=aws_fork_url,
         json={
             "jsonrpc": "2.0",
             "method": "hardhat_setStorageAt",
