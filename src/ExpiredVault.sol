7bdb00047635c9d362d8b61c45045e4c4983359e
sacredwx <sacredwx@gmail.com>
2023-11-20 16:37:22 +0400

    [Add] ExpiredVault Contract

---
+++ src/ExpiredVault.sol
@@ -0,0 +1,64 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity >=0.8.21;
+
+import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import { AccessControlUpgradeable } from "openzeppelin-contracts-upgradeable/access/AccessControlUpgradeable.sol";
+import "./interfaces/ILeverageEngine.sol";
+import "./interfaces/IExpiredVault.sol";
+import "./PositionLedgerLib.sol";
+
+/// @title ExpiredVault Contract
+/// @notice This contract holds the expired positions' funds and enables withdrawal of funds by users
+/// against the NFT representing their position.
+/// @dev This contract is upgradeable
+contract ExpiredVault is IExpiredVault, AccessControlUpgradeable {
+    using SafeERC20 for IERC20;
+
+    // Define roles
+    bytes32 public constant MONITOR_ROLE = keccak256("MONITOR_ROLE");
+
+    // WBTC token
+    IERC20 public wbtc = IERC20(0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599);
+
+    // Leverage Engine
+    ILeverageEngine leverageEngine;
+
+    constructor() {
+        _disableInitializers();
+    }
+
+    function initialize(address _leverageEngine) external initializer {
+        __AccessControl_init();
+        leverageEngine = ILeverageEngine(_leverageEngine);
+        _grantRole(MONITOR_ROLE, _leverageEngine);
+    }
+
+    ///////////// Monitor functions /////////////
+
+    /// @notice Deposits WBTC into the vault from expired or liquidated positions.
+    /// @param amount Amount of WBTC to deposit into the vault.
+    function deposit(uint256 amount) external onlyRole(MONITOR_ROLE) {
+        wbtc.safeTransferFrom(msg.sender, address(this), amount);
+    }
+
+    ///////////// User functions /////////////
+
+    /// @notice Allows users to claim their WBTC based on their position.
+    /// @param nftID The ID of the NFT representing the position.
+    function claim(uint256 nftID) external {
+        PositionLedgerLib.LedgerEntry memory position = leverageEngine.getPosition(nftID);
+
+        require(wbtc.balanceOf(address(this)) >= position.claimableAmount, "Not enough WBTC in the vault");
+
+        // Transfer the claimable amount to the user
+        // TODO reentrancy
+        if (position.claimableAmount > 0) {
+            wbtc.safeTransfer(msg.sender, position.claimableAmount);
+        }
+
+        // Update the ledger entry for this position
+        // Checks the ownership of the NFT and reverts if the caller is not the owner
+        // Checks the position state as well
+        leverageEngine.closeExpiredPosition(nftID, msg.sender);
+    }
+}
