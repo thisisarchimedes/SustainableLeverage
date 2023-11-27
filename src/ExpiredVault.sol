0ed5c1d57577b9c6169527941e331ef5e503a5bd
sacredwx <sacredwx@gmail.com>
2023-11-27 20:03:12 +0400

    [Add] Errors Checks to Expired Vault

--- src/ExpiredVault.sol
+++ src/ExpiredVault.sol
@@ -3,7 +3,7 @@ pragma solidity >=0.8.21;
 
 import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import { AccessControlUpgradeable } from "openzeppelin-contracts-upgradeable/access/AccessControlUpgradeable.sol";
-import "./interfaces/ILeverageEngine.sol";
+import "./LeverageEngine.sol";
 import "./interfaces/IExpiredVault.sol";
 import "./PositionLedgerLib.sol";
 
@@ -16,10 +16,12 @@ contract ExpiredVault is IExpiredVault, AccessControlUpgradeable {
 
     // Errors
     error InsufficientFunds();
+    error NotOwner();
+    error PositionNotExpiredOrLiquidated();
 
     // Events
     event Deposit(address indexed depositor, uint256 amount);
-    event Claim(address indexed claimer, uint256 indexed nftID, uint256 amount);
+    event Claim(address indexed claimer, uint256 indexed nftId, uint256 amount);
 
     // Define roles
     bytes32 public constant MONITOR_ROLE = keccak256("MONITOR_ROLE");
@@ -31,7 +33,7 @@ contract ExpiredVault is IExpiredVault, AccessControlUpgradeable {
     uint256 public balance;
 
     // Leverage Engine
-    ILeverageEngine public leverageEngine;
+    LeverageEngine public leverageEngine;
 
     constructor() {
         _disableInitializers();
@@ -39,8 +41,7 @@ contract ExpiredVault is IExpiredVault, AccessControlUpgradeable {
 
     function initialize(address _leverageEngine, address _wbtc) external initializer {
         __AccessControl_init();
-        wbtc = IERC20(0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599);
-        leverageEngine = ILeverageEngine(_leverageEngine);
+        leverageEngine = LeverageEngine(_leverageEngine);
         _grantRole(MONITOR_ROLE, _leverageEngine);
         wbtc = IERC20(_wbtc);
     }
@@ -63,9 +64,18 @@ contract ExpiredVault is IExpiredVault, AccessControlUpgradeable {
     ///////////// User functions /////////////
 
     /// @notice Allows users to claim their WBTC based on their position.
-    /// @param nftID The ID of the NFT representing the position.
-    function claim(uint256 nftID) external {
-        PositionLedgerLib.LedgerEntry memory position = leverageEngine.getPosition(nftID);
+    /// @param nftId The ID of the NFT representing the position.
+    function claim(uint256 nftId) external {
+        PositionLedgerLib.LedgerEntry memory position = leverageEngine.getPosition(nftId);
+
+        // Check if the user owns the NFT
+        if (leverageEngine.nft().ownerOf(nftId) != msg.sender) revert NotOwner();
+
+        // Check if the NFT state is Expired or Liquidated
+        if (
+            position.state != PositionLedgerLib.PositionState.EXPIRED
+                && position.state != PositionLedgerLib.PositionState.LIQUIDATED
+        ) revert PositionNotExpiredOrLiquidated();
 
         if(balance < position.claimableAmount) revert InsufficientFunds();
 
@@ -75,7 +85,7 @@ contract ExpiredVault is IExpiredVault, AccessControlUpgradeable {
         // Update the ledger entry for this position
         // Checks the ownership of the NFT and reverts if the caller is not the owner
         // Checks the position state as well
-        leverageEngine.closeExpiredPosition(nftID, msg.sender);
+        leverageEngine.closeExpiredOrLiquidatedPosition(nftId, msg.sender);
 
         // Transfer the claimable amount to the user
         if (position.claimableAmount > 0) {
@@ -83,6 +93,6 @@ contract ExpiredVault is IExpiredVault, AccessControlUpgradeable {
         }
 
         // Emit event
-        emit Claim(msg.sender, nftID, position.claimableAmount);
+        emit Claim(msg.sender, nftId, position.claimableAmount);
     }
 }
