e332150e787616b5fa991a92e81472df26b7e433
tomerXarchimedes <tomer@archimedesfi.com>
2023-12-25 08:05:48 -0500

    Create load_eth_balance.py

---
+++ script/hardhat_helpers/load_eth_balance.py
@@ -0,0 +1,25 @@
+import requests
+import os
+import sys
+
+
+def set_eth_balance_of_address(address, amount):
+    # The line below automatiaclly updates by Github Actions using sed (this script is called via Foundry and doesn't have env vars)
+    fork_url = "http://localhost:8545" #THE_FORK_URL
+    requests.post(
+        url=fork_url,
+        json={
+            "jsonrpc": "2.0",
+            "method": "hardhat_setBalance",
+            "params": [address, amount],
+            "id": 67,
+        },
+    )
+
+
+def main():
+    args = sys.argv[1:]
+    return set_eth_balance_of_address(*args)
+
+
+__name__ == "__main__" and main()
