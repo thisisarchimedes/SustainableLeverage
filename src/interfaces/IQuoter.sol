4e7ba8c17629ec7470b7c42eb73f99bc2a588c5a
danielj86 <danieljafe@gmail.com>
2024-01-11 20:22:52 +0200

    Delete IQuoter.sol

--- src/interfaces/IQuoter.sol
+++
@@ -1,7 +0,0 @@
-    // SPDX-License-Identifier: UNLICENSED
-pragma solidity >=0.8.21;
-pragma abicoder v2;
-
-interface IQuoter {
-    function quoteExactInput(bytes memory path, uint256 amountIn) external view returns (uint256 amountOut);
-}
